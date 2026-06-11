04bdca9e3cc45036c26ae8bb0f89da4b9cbd90e5
David1212 <160751928+davidcito1212@users.noreply.github.com>
2026-06-11 13:52:44 -0500

    feat: crear rutas y controlador de tareas

---
+++ app/Http/Controllers/TareaController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarea;
+use Illuminate\Http\Request;
+
+class TareaController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit(Tarea $tarea)
+    {
+
+    }
+
+    public function update(Request $request, Tarea $tarea)
+    {
+
+    }
+
+    public function destroy(Tarea $tarea)
+    {
+
+    }
+
+    public function toggle(Tarea $tarea)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tareas', [TareaController::class, 'index'])
+    ->name('tareas.index');
+
+Route::get('/tareas/crear', [TareaController::class, 'create'])
+    ->name('tareas.create');
+
+Route::post('/tareas', [TareaController::class, 'store'])
+    ->name('tareas.store');
+
+Route::get('/tareas/{tarea}/editar', [TareaController::class, 'edit'])
+    ->name('tareas.edit');
+
+Route::put('/tareas/{tarea}', [TareaController::class, 'update'])
+    ->name('tareas.update');
+
+Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])
+    ->name('tareas.destroy');
+
+Route::patch('/tareas/{tarea}/toggle', [TareaController::class, 'toggle'])
+    ->name('tareas.toggle');
